0c562dfedc2550a2d5d34ba669771d0c0654f2ae
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-08-29 18:38:54 +0200

    a

--- code/config/hashing.php
+++ code/config/hashing.php
@@ -3,19 +3,16 @@
     [
         'driver' => 'argon',
 
-
         'bcrypt' =>
         [
             'rounds' => env('BCRYPT_ROUNDS', 10),
         ],
 
-
         'argon' =>
         [
-            'memory' => 65536,
-            'threads' => 1,
-            'time' => 4,
+            'memory'  => 65536,
+            'threads' => 2,
+            'time'    => 8,
         ],
-
     ];
 ?>
