6306a9c0fee6348dababe89bc463e5468b3435a7
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-08-30 20:45:01 +0200

    adding routes

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,9 +1,15 @@
 <?php
+    require_once './api/versions.php';
+
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Route;
 
 
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::middleware('auth:sanctum')->get(
+        '/user',
+        function ( Request $request )
+        {
+            return $request->user();
+        }
+    );
 ?>
