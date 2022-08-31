21778d510cc3db95a4321b1c876afaae84ad061e
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-08-31 10:06:46 +0200

    reformating code

--- code/app/Http/Controllers/LinkModelController.php
+++ code/app/Http/Controllers/LinkModelController.php
@@ -1,70 +1,47 @@
 <?php
+    namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
+    use App\Models\LinkModel;
+    use App\Http\Requests\StoreLinkModelRequest;
+    use App\Http\Requests\UpdateLinkModelRequest;
+    use Illuminate\Http\Response;
 
-use App\Models\LinkModel;
-use App\Http\Requests\StoreLinkModelRequest;
-use App\Http\Requests\UpdateLinkModelRequest;
 
-class LinkModelController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    class LinkModelController
+        extends Controller
     {
-        //
-        return null;
-    }
+        public function index(): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreLinkModelRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreLinkModelRequest $request)
-    {
-        //
-        return null;
-    }
+        public function store( StoreLinkModelRequest $request ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\LinkModel  $linkModel
-     * @return \Illuminate\Http\Response
-     */
-    public function show(LinkModel $linkModel)
-    {
-        //
-        return null;
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateLinkModelRequest  $request
-     * @param  \App\Models\LinkModel  $linkModel
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateLinkModelRequest $request, LinkModel $linkModel)
-    {
-        //
-        return null;
-    }
+        public function show( LinkModel $linkModel ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\LinkModel  $linkModel
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(LinkModel $linkModel)
-    {
-        //
-        return null;
+
+        public function update( UpdateLinkModelRequest $request,
+                                LinkModel $linkModel ): ?Response
+        {
+            //
+            return null;
+        }
+
+
+        public function destroy( LinkModel $linkModel ): ?Response
+        {
+            //
+            return null;
+        }
     }
-}
+?>
