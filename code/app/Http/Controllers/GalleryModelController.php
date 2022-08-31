0e129877c0d52100484bb8bf538c4c68905f4f5e
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-08-31 10:02:20 +0200

    reformating code

--- code/app/Http/Controllers/GalleryModelController.php
+++ code/app/Http/Controllers/GalleryModelController.php
@@ -1,70 +1,45 @@
 <?php
+    namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
+    use App\Models\GalleryModel;
+    use App\Http\Requests\StoreGalleryModelRequest;
+    use App\Http\Requests\UpdateGalleryModelRequest;
+    use Illuminate\Http\Response;
 
-use App\Models\GalleryModel;
-use App\Http\Requests\StoreGalleryModelRequest;
-use App\Http\Requests\UpdateGalleryModelRequest;
 
-class GalleryModelController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    class GalleryModelController
+        extends Controller
     {
-        //
-        return null;
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreGalleryModelRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreGalleryModelRequest $request)
-    {
-        //
-        return null;
-    }
+        public function index()
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\GalleryModel  $galleryModel
-     * @return \Illuminate\Http\Response
-     */
-    public function show(GalleryModel $galleryModel)
-    {
-        //
-        return null;
-    }
+        public function store( StoreGalleryModelRequest $request ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateGalleryModelRequest  $request
-     * @param  \App\Models\GalleryModel  $galleryModel
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateGalleryModelRequest $request, GalleryModel $galleryModel)
-    {
-        //
-        return null;
-    }
+        public function show( GalleryModel $galleryModel ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\GalleryModel  $galleryModel
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(GalleryModel $galleryModel)
-    {
-        //
-        return null;
+        public function update( UpdateGalleryModelRequest $request,
+                                GalleryModel $galleryModel ): ?Response
+        {
+            //
+            return null;
+        }
+
+        public function destroy( GalleryModel $galleryModel ): ?Response
+        {
+            //
+            return null;
+        }
     }
-}
+?>
--- code/app/Http/Controllers/ImageModelController.php
+++ code/app/Http/Controllers/ImageModelController.php
@@ -1,71 +1,44 @@
 <?php
+    namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
+    use App\Models\ImageModel;
+    use App\Http\Requests\StoreImageModelRequest;
+    use App\Http\Requests\UpdateImageModelRequest;
+    use Illuminate\Http\Response;
 
-use App\Models\ImageModel;
-use App\Http\Requests\StoreImageModelRequest;
-use App\Http\Requests\UpdateImageModelRequest;
-use Illuminate\Http\Response;
 
-class ImageModelController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(): ?Response
+    class ImageModelController
+        extends Controller
     {
-        //
-        return null;
-    }
+        public function index(): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreImageModelRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreImageModelRequest $request)
-    {
-        //
-        return null;
-    }
+        public function store( StoreImageModelRequest $request ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ImageModel  $imageModel
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ImageModel $imageModel)
-    {
-        //
-        return null;
-    }
+        public function show( ImageModel $imageModel ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateImageModelRequest  $request
-     * @param  \App\Models\ImageModel  $imageModel
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateImageModelRequest $request, ImageModel $imageModel)
-    {
-        //
-        return null;
-    }
+        public function update( UpdateImageModelRequest $request,
+                                ImageModel $imageModel ): ?Response
+        {
+            //
+            return null;
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ImageModel  $imageModel
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(ImageModel $imageModel)
-    {
-        //
-        return null;
+        public function destroy( ImageModel $imageModel ): ?Response
+        {
+            //
+            return null;
+        }
     }
-}
+?>
